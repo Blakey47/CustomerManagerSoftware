aeedc2e62922338aa0062ad8502c4a418e505530
Darragh Blake <darragh.blake48@gmail.com>
2018-10-22 17:51:23 +0100

    Added a Service Layer

---
+++ src/com/darraghblake/customermanagersoftware/service/CustomerService.java
@@ -0,0 +1,10 @@
+package com.darraghblake.customermanagersoftware.service;
+
+import java.util.List;
+
+import com.darraghblake.customermanagersoftware.entity.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+}
