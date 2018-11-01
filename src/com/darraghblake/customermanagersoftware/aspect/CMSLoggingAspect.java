5860c8bd03e37792e12f3a381241cfed776a04dd
Darragh Blake <darragh.blake48@gmail.com>
2018-11-01 16:23:26 +0000

    Add @Before Advice Type

--- src/com/darraghblake/customermanagersoftware/aspect/CMSLoggingAspect.java
+++ src/com/darraghblake/customermanagersoftware/aspect/CMSLoggingAspect.java
@@ -2,7 +2,9 @@ package com.darraghblake.customermanagersoftware.aspect;
 
 import java.util.logging.Logger;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
@@ -14,16 +16,23 @@ public class CMSLoggingAspect {
 	private Logger myLogger = Logger.getLogger(getClass().getName());
 	
 	// Setup @Pointcut declarations
-	@Pointcut("execution(* com.darraghblake.customermanagersoftware.controller.*.*.(..))")
+	@Pointcut("execution(* com.darraghblake.customermanagersoftware.controller.*.*(..))")
 	private void forControllerPackage() {}
 	
-	@Pointcut("execution(* com.darraghblake.customermanagersoftware.service.*.*.(..))")
+	@Pointcut("execution(* com.darraghblake.customermanagersoftware.service.*.*(..))")
 	private void forServicePackage() {}
 	
-	@Pointcut("execution(* com.darraghblake.customermanagersoftware.dao.*.*.(..))")
+	@Pointcut("execution(* com.darraghblake.customermanagersoftware.dao.*.*(..))")
 	private void forDAOPackage() {}
 	
 	@Pointcut("forControllerPackage() || forServicePackage() || forDAOPackage()")
 	private void forAppFlow() {}
 	
+	
+	// Add @Before Advice Type
+	@Before("forAppFlow()")
+	public void before(JoinPoint theJoinPoint) {
+		String theMethod = theJoinPoint.getSignature().toShortString();
+		myLogger.info("------> in @Before: calling method: " + theMethod);
+	}
 }
